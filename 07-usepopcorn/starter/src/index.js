5b64257987d03cb8f457aa3e540c289921f0f604
Mostafa <mostafaasd2018@gmail.com>
2023-07-27 11:20:26 +0300

    "usePopCorn" Project
    - Split the App component to many small reusable components
    - create the reusable StarRating Component in a separate file.

--- 07-usepopcorn/starter/src/index.js
+++ 07-usepopcorn/starter/src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from  "./App";
+import Stars from "./StarsRating"
+
+function Test() {
+  const [rating, setRating] = React.useState(0);
+  return (
+    <div>
+      <Stars color="red" size={70} onSetRating={setRating}></Stars>
+      <h3> You Chosed {rating} starts </h3>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <Stars />
+
+    <Stars size={40} color="blue" maxRating={10} ></Stars>
+
+    <Test></Test>
+
   </React.StrictMode>
 );
