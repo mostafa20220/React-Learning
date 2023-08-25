d16df0a8885d0644d02f4a09fae7f05496c15b46
Mostafa <mostafaasd2018@gmail.com>
2023-08-25 15:47:19 +0300

    Optimize the Usage of Context API
    - By creating Custom Context Provider and Custom useHook.

---
+++ 12-atomic-blog/starter/src/PostContext.js
@@ -0,0 +1,53 @@
+import React, { createContext, useContext, useState } from "react";
+import CreateRandomPost from "./CreateRandomPost";
+
+const PostContext = createContext();
+
+// Custom Context Provider
+export function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => CreateRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+// Custom useContext Hook
+export function usePosts() {
+  const context = useContext(PostContext);
+
+  if (context === undefined)
+    throw new Error("usePost was used outside PostContext Children!");
+  return context;
+}
