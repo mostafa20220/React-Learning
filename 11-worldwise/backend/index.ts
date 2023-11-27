39fc9955a0796d64de580e9c1ca24e76b15f5a04
Mostafa <mostafaasd2018@gmail.com>
2023-11-27 21:30:48 +0200

    update CORS policy

--- 11-worldwise/backend/index.ts
+++ 11-worldwise/backend/index.ts
@@ -13,17 +13,15 @@ import { AppError, createRes } from "./src/utils/helpers";
 import { usersRouter } from "./src/routers/users.router";
 import { authRouter } from "./src/routers/auth.router";
 
-
 // const options = {
-  // key: fs.readFileSync('localhost-key.pem'),
-  // cert: fs.readFileSync('localhost.pem'),
+// key: fs.readFileSync('localhost-key.pem'),
+// cert: fs.readFileSync('localhost.pem'),
 // };
 
 const app = express();
 
 // const server = https.createServer(options, app);
 const server = https.createServer(app);
-
 
 // connect mongodb
 (function connectMongoose() {
@@ -40,13 +38,13 @@ app.use("/api/uploads", express.static(path.join(__dirname, "uploads"))); // use
 app.use(express.json()); // use express bodyParser as middleware
 app.use(cookieParser()); // use cookieParser as middleware
 app.use(morgan("dev")); // use morgan as middleware
-app.use(cors({ origin: "https://worldwide-react-app.onrender.com", credentials: true }));
-
+// app.use(cors({ origin: "https://worldwide-react-app.onrender.com", credentials: true }));
+app.use(cors({ credentials: true }));
 
 // Routers
 app.use("/api/cities", citiesRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/auth", authRouter); 
+app.use("/api/auth", authRouter);
 
 // Handling wrong routers
 app.all("*", (req, res, next) => {
@@ -58,13 +56,13 @@ app.all("*", (req, res, next) => {
 app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
   fs.appendFileSync(
     "errors.log",
-    new Date().toLocaleString() + "\t" + err.message + "\n",
+    new Date().toLocaleString() + "\t" + err.message + "\n"
   );
   // console.log(err);
   const code = err.code && err.code >= 100 && err.code < 600 ? err.code : 500;
   res
     .status(code)
-    .json(createRes(err.statusText || "error", err.message,code));
+    .json(createRes(err.statusText || "error", err.message, code));
 });
 
 // lunch the server
